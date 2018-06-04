bc4611ecd4c28c03487777922d87eb7ba3cfe7c3
Kashmira Pardeshi <pardeshikashmira@gmail.com>
2018-06-03 20:09:08 -0400

    function on the first button to transfer the text from left to right

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ document.getElementById("btn1").addEventListener("click", left);
 document.getElementById("btn2").addEventListener("click", pause);
 document.getElementById("btn3").addEventListener("click", right);
 
+//adding interval function and left function.
+var interval;
+
+function left(){
+	clearInterval(interval);
+	interval = setInterval(function(){
+			var text1 = document.getElementById('text1').value;
+			var x = document.getElementById('text2').value += text1.substring(0,1);
+			document.getElementById('text1').value = text1.substring(1);
+	},1000);	
+}
