5fc662bd1031ba5014d694f33c66eb8e31f4c95e
Kashmira Pardeshi <pardeshikashmira@gmail.com>
2018-06-03 20:07:56 -0400

    added layout in the html file and just added event listners in the js file

---
+++ app.js
@@ -0,0 +1,6 @@
+//adding event listerners
+
+document.getElementById("btn1").addEventListener("click", left);
+document.getElementById("btn2").addEventListener("click", pause);
+document.getElementById("btn3").addEventListener("click", right);
+
